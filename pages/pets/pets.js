9a31205139d0c75297e54214379efe86fa7c7baf
Valeriy <v4ler0nnch@gmail.com>
2022-05-23 18:39:11 +0300

    finished burger menu | main and our-pets pages

--- pages/pets/pets.js
+++ pages/pets/pets.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector(".hambueger__menu");
 const listMenu = document.querySelector(".header__list");
+const body = document.body;
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     listMenu.classList.toggle("active");
+    body.classList.add("disable__scroll");
 })
 
 document.querySelectorAll(".header__list").forEach(n => n.addEventListener("click",() => {
     hamburger.classList.remove("active");
     listMenu.classList.remove("active");
+    body.classList.remove("disable__scroll");
+    body.classList.add("enable__scroll");
 }))
